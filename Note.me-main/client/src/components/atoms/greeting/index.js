69a86c307706f18b79a12fedec52faa906df7050
Prachi12215421 <119073607+prachi-2627@users.noreply.github.com>
2024-07-25 21:15:00 +0530

    Add files via upload

---
+++ Note.me-main/client/src/components/atoms/greeting/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./greeting.module.scss";
+
+function Greeting() {
+  return (
+    <section className={styles.container}>
+      <h2>
+        Hello, <span>Prachi! 👋</span>
+      </h2>
+      <p>All your notes are here, in one place!</p>
+    </section>
+  );
+}
+
+export default Greeting;
